a89904e88869c6babd4a276425fe0d8fd63eee7b
Gulam Anas <g.anas786@gmail.com>
2022-01-17 18:32:13 +0530

    initial restaurant repos

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
