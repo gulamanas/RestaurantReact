6dc402ed4057ac3f519642f844bbe924b51d91ab
Gulam Anas <g.anas786@gmail.com>
2022-01-18 09:03:45 +0530

    fixed navbar error

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,18 +12,18 @@ function Nav() {
     return (
         <nav className='navbar'>
             <div className="navbar_brand">
-                <Link to="/">
+                <Link to="/" className='brand'>
                     RESTRO <i className="fas fa-utensils"></i>
                 </Link>
             </div>
             <div className="burger" onClick={handleBurgerMenu}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
-            <ul className="nav_list">
-                <li><Link to='home'>Home</Link></li>
-                <li><Link to='menu'>Menu</Link></li>
-                <li><Link to='about'>About</Link></li>
-                <li><Link to='contact'>Contact</Link></li>
+            <ul className={click ? "nav_list active" : 'nav_list'}>
+                <li><Link to='home' className='list_item'>Home</Link></li>
+                <li><Link to='menu' className='list_item'>Menu</Link></li>
+                <li><Link to='about' className='list_item'>About</Link></li>
+                <li><Link to='contact' className='list_item'>Contact</Link></li>
             </ul>
 
         </nav>
