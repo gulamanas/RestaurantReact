64b1900e4121b5f677bae8eb2c5ef17c19008b1c
Gulam Anas <g.anas786@gmail.com>
2022-01-19 21:58:50 +0530

    created cards file

---
+++ src/components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Cards() {
+    return (
+        <div className='cards'>
+            <div className="cards_wrapper">
+                <div className="cards_img">
+                    <img src="" alt="" />
+                </div>
+                <div className="cards_content">
+                    <p className='cards_item_text'></p><span className='cards_item_price'></span>
+                    <p className='cards_item_details'></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import './Hero.css'
 function Hero() {
     return (
         <div className='main_hero'>
-            <h1>Have you tried <br />our startes yet? <br />Visit us today!</h1>
+            <h1>Have you tried <br />our starters yet? <br />Visit us today!</h1>
             <button className='hero_btn'>Try Now</button>
         </div>
     )
